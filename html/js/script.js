deed6ea30def548794dee4a98bb910bef0e176d8
masterdev37 <masterdev37@gmail.com>
2023-11-21 13:28:51 +0600

    start doc

---
+++ html/js/script.js
@@ -0,0 +1,205 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+	new WOW().init();
+
+	// Header START
+	$('.hamburger').on('click', function() {
+		if($(this).hasClass('is-active')) {
+			$(this).removeClass('is-active');
+			$('.header-mobile-wrap').slideUp(500);
+		}
+		else {
+			$(this).addClass('is-active');
+			$('.header-mobile-wrap').slideDown(500);
+		}
+	});
+
+	function scrollHeader() {
+		let headerTopHeight = $('.header-top').height();
+		if($(this).scrollTop() > headerTopHeight) {
+			$('.header-bottom').addClass('is-fixed');
+		}
+		else {
+			$('.header-bottom').removeClass('is-fixed');
+		}
+	}
+	function showArrowUp() {
+		if($(this).scrollTop() > 1500) {
+			$('.go-up').addClass('is-active');
+		}
+		else {
+			$('.go-up').removeClass('is-active');
+		}
+	}
+
+	// Counter num START
+	  function isElementInViewport(el) {
+		const rect = el.getBoundingClientRect();
+		return (
+		  rect.top >= 0 &&
+		  rect.left >= 0 &&
+		  rect.bottom <= ($(window).height() || $(document.documentElement).height()) &&
+		  rect.right <= ($(window).width() || $(document.documentElement).width())
+		);
+	  }
+	
+	  function handleScroll() {
+		const targetElement = $(".s-numbers");
+		if (isElementInViewport(targetElement[0])) {
+		  targetElement.find(".numbers-num").each(function() {
+			const targetCount = parseInt($(this).data("count"));
+			const duration = 2000; // Измените время анимации по вашему усмотрению
+			const frameDuration = 1000 / 60;
+			const totalFrames = Math.round(duration / frameDuration);
+			const countIncrement = targetCount / totalFrames;
+	
+			let currentCount = 0;
+			const counter = setInterval(() => {
+			  currentCount += countIncrement;
+			  if (currentCount >= targetCount) {
+				currentCount = targetCount;
+				clearInterval(counter);
+			  }
+			  $(this).text(Math.round(currentCount));
+			}, frameDuration);
+		  });
+		  $(window).off("scroll", handleScroll);
+		}
+	  }
+	  $(window).on("scroll", handleScroll);
+	  handleScroll();
+	// Counter num END
+
+
+	$(window).on('scroll', function() {
+		scrollHeader();
+		showArrowUp();
+	});
+	scrollHeader();
+	showArrowUp();
+
+	$('.anchor-link').on('click', function () {
+	    let href = $(this).attr('href');
+
+	    $('html, body').animate({
+	        scrollTop: $(href).offset().top
+	    }, {
+	        duration: 700,
+	    });
+		$('.header-mobile-wrap').slideUp(500);
+		$('.hamburger').removeClass('is-active');
+	    return false;
+	});
+
+	$('.go-up').on('click', function () {
+	    $('html, body').animate({
+	        scrollTop: 0
+	    }, {
+	        duration: 700,
+	    });
+	    return false;
+	});
+	// Header END
+	
+	// Banner START
+	const bannerSwiper = new Swiper('.banner-swiper', {
+		speed: 1000,
+		spaceBetween: 0,
+		autoHeight: true,
+		navigation: {
+			nextEl: '.banner-swiper .swiper-button-next',
+			prevEl: '.banner-swiper .swiper-button-prev',
+		},
+		pagination: {
+			el: '.banner-swiper .swiper-pagination',
+			type: 'bullets',
+			clickable: true,
+		},
+	});
+	// Banner END
+
+
+	// Services START
+	$('.services-btn').magnificPopup({
+		type: 'inline',
+		showCloseBtn: false,
+		removalDelay: 500,
+		callbacks: {
+			beforeOpen: function() {
+			   this.st.mainClass = this.st.el.attr('data-effect');
+			}
+		},
+	});
+	$('.modal-form-close').on('click', function() {
+		$.magnificPopup.close();
+	});
+	// Services END
+
+	// Gallery START
+	$('.gallery-wrap a').magnificPopup({
+		type: 'image',
+		gallery: {
+			enabled: true
+		},
+		callbacks: {
+			beforeOpen: function() {
+				this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+				this.st.mainClass = this.st.el.attr('data-effect');
+			}
+		},
+	});
+
+	$('.gallery-btn a').on('click', function(e) {
+		e.preventDefault();
+		var galleryItem = $('.gallery-item');
+
+		if($(this).hasClass('is-active')) {
+			$(this).removeClass('is-active');
+			$(this).text('Show more');
+			galleryItem.each(function() {
+				if($(this).hasClass('is-active')) {
+					$(this).removeClass('is-active');
+					$(this).slideUp();
+				}
+			});
+		}
+		else {
+			$(this).addClass('is-active');
+			$(this).text('Hide');
+			galleryItem.each(function() {
+				if(!$(this).is(':visible')) {
+					$(this).addClass('is-active');
+					$(this).slideDown();
+				}
+			});
+		}
+	});
+	// Gallery END
+
+
+	// Reviews START
+	const reviewsSwiper = new Swiper('.reviews-swiper', {
+		speed: 1000,
+		spaceBetween: 20,
+		pagination: {
+			el: '.reviews-swiper .swiper-pagination',
+			type: 'bullets',
+			clickable: true,
+		},
+		breakpoints: {
+			320: {
+				slidesPerView: 1,
+			},
+			575: {
+				slidesPerView: 2,
+			},
+			992: {
+				slidesPerView: 3,
+			},
+		  }
+	});
+	// Reviews END
+
+	
+
+})
